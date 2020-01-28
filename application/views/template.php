fd61d6b004dee78cb6e6fda369ee9d5920ad1a62
fangkiyudho <fangkiyudho@gmail.com>
2020-01-29 02:49:34 +0700

    Dashboard repair

--- application/views/template.php
+++ application/views/template.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>GARUDA CRUD Generator</title>
+        <title>Arsip Surat</title>
         <!-- Tell the browser to be responsive to screen width -->
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <!-- Bootstrap 3.3.7 -->
@@ -40,9 +40,9 @@
                 <!-- Logo -->
                 <a href="<?php echo base_url() ?>adminlte/index2.html" class="logo">
                     <!-- mini logo for sidebar mini 50x50 pixels -->
-                    <span class="logo-mini"><b>A</b>LT</span>
+                    <span class="logo-mini"><b>A</b>S</span>
                     <!-- logo for regular state and mobile devices -->
-                    <span class="logo-lg"><b>BROCODE</b>LTE</span>
+                    <span class="logo-lg"><b>Arsip Surat</b></span>
                 </a>
                 <!-- Header Navbar: style can be found in header.less -->
                 <nav class="navbar navbar-static-top">
@@ -71,7 +71,7 @@
 
                                         <p>
                                             <?php echo $this->session->userdata('full_name'); ?>                                         
-                                            <small>Member since Nov. 2012</small>
+                                            <small>Dinas Penidikan Kota Batu</small>
                                         </p>
                                     </li>
                                     <!-- Menu Footer-->
